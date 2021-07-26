33ed120b635f6f3182d151412e098d17ab1811e6
Chris Bates <47527638+L3llantos@users.noreply.github.com>
2021-07-26 22:55:07 +0100

    added disable animation toggle

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
 function rollDice() {
+    //hide old results
     document.getElementById("rollResults").style.visibility = "hidden";
-    var dieCount = document.getElementById("diceAmount").value;
-    var i = 0;
-    const rolls = new Array();
-    setTimeout(function () {
-        document.getElementById("rollResults").style.visibility = "visible";
-    }, 7250);
-    var video = document.getElementById("rolledVideo")
 
-    video.play();
-    //count wins or fails
+    var ani = document.getElementById("disableAnimations").checked;
+
+    if (ani === true) {
+        setTimeout(function () {
+            document.getElementById("rollResults").style.visibility = "visible";
+        }, 17250);
+        var video = document.getElementById("rolledVideo")
+
+        video.play();
+    }
+
+
+    var dieCount = document.getElementById("diceAmount").value;
+
+    var i = 0;
+
+    const rolls = new Array();
+
+
     function getOccurrence(array, value) {
         return array.filter((v) => (v === value)).length;
     }
@@ -25,20 +36,20 @@ function rollDice() {
         i++;
 
     }
-
+    //count wins and fails
     function countWins(wins) {
         x = getOccurrence(rolls, 5);
         y = getOccurrence(rolls, 6);
         wins = x + y;
         return wins;
     }
-
     document.getElementById("fails").innerHTML = getOccurrence(rolls, 1);
-
+    //end
+    //display results
     document.getElementById("wins").innerHTML = countWins();
-
     document.getElementById("Result").innerHTML = rolls;
+    document.getElementById("rollResults").style.visibility = "visible";
     console.log(rolls);
-
-
 }
+
+
